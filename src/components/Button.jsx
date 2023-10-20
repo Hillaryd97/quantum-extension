39695addd66e2acbe030ee64504deb0442bf9d46
Hillary <Hillaryd97@gmail.com>
2023-10-20 14:53:41 +0100

    .

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import chromeLogo from "../assets/google-chrome-icon-20111.png";
 const Button = () => {
   return (
     <div>
-      <a href="/assets/quantum-extension.zip" download={"quantum-extension.zip"} className="px-6 py-4 bg-quantum_500 text-white rounded-lg font-medium text-base hover:bg-opacity-90 flex w-fit space-x-6">
+      <a href="https://github.com/Hillaryd97/quantum-extension/blob/main/src/assets/quantum-extension.zip" download={"quantum-extension.zip"} className="px-6 py-4 bg-quantum_500 text-white rounded-lg font-medium text-base hover:bg-opacity-90 flex w-fit space-x-6">
         <img src={chromeLogo} alt="" />
         <p>Download Extension</p>
       </a>
